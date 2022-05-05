9907cc8421b22590eededa9f178db99cd73b11ea
Ariel <arielanus1980@yahoo.com.ar>
2022-05-05 00:33:03 -0300

    subo archivos HTML y JavaScript

---
+++ posicion.js
@@ -0,0 +1,7 @@
+var posicionEnLaCarrera = 6
+if (posicionEnLaCarrera===1){console.log("Felicitaciones, saliste 1ro")}
+if (posicionEnLaCarrera===2){console.log("Te falto poquito para ganar")}
+if (posicionEnLaCarrera===3){console.log("Sos un orgullo para tu familia")}
+if (posicionEnLaCarrera>3){console.log("Volve a tu casa")}
+if (posicionEnLaCarrera<=0){console.log("No tires fruta")}
+
